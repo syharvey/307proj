83e1cf22234c571dc734895ea504fc28693a91ce
Syrsha Harvey <syharvey@calpoly.edu>
2020-04-16 12:34:10 -0700

    added ability to post users

--- src/App.js
+++ src/App.js
@@ -19,7 +19,11 @@ class App extends Component {
     }
 
     handleSubmit = character => {
-      this.setState({ characters: [...this.state.characters, character] })
+       this.makePostCall(character).then(callResult => {
+          if (callResult === true) {
+	     this.setState({ characters: [...this.state.characters, character] });
+	   }
+    });
     }
 
     render() {
@@ -32,6 +36,7 @@ class App extends Component {
         </div>
       )
     }
+
     componentDidMount() {
        axios.get('http://localhost:5000/users')
        .then(res => {
@@ -42,7 +47,18 @@ class App extends Component {
             //Not handling the error. Just logging into the console.
           console.log(error);
     	});
-    } 
+    }
+	makePostCall(character){
+		return axios.post('http://localhost:5000/users', character)
+		.then(function (response) {
+			console.log(response);
+			return (response.status === 200);
+		})
+		.catch(function (error) {
+      			console.log(error);
+      			return false;
+    		});
+ 	}
   }
 
 export default App
