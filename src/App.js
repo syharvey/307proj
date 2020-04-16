afda969e5a3a1ed8a46731e14dbf93818f45bb2a
Syrsha Harvey <syharvey@calpoly.edu>
2020-04-16 11:44:27 -0700

    added componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 class App extends Component {
     state = {
@@ -25,12 +26,23 @@ class App extends Component {
       const { characters } = this.state
     
       return (
-        <div className="container">
+      	<div className="container">
           <Table characterData={characters} removeCharacter={this.removeCharacter} />
           <Form handleSubmit={this.handleSubmit} />
         </div>
       )
     }
+    componentDidMount() {
+       axios.get('http://localhost:5000/users')
+       .then(res => {
+          const characters = res.data.users_list;
+          this.setState({ characters });
+       })
+       .catch(function (error) {
+            //Not handling the error. Just logging into the console.
+          console.log(error);
+    	});
+    } 
   }
 
 export default App
